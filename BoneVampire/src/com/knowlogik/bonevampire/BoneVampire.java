f263937e2774785f197f16c248684f18435bcdf6
Naim Falandino <lnfalandino@gmail.com>
2012-04-01 21:49:56 -0400

    Window improvements

--- BoneVampire/src/com/knowlogik/bonevampire/BoneVampire.java
+++ BoneVampire/src/com/knowlogik/bonevampire/BoneVampire.java
@@ -2,12 +2,17 @@ package com.knowlogik.bonevampire;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
+import java.awt.Window;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import org.OpenNI.Context;
-import org.OpenNI.GeneralException;
-import org.OpenNI.OutArg;
-import org.OpenNI.ScriptNode;
-import org.OpenNI.UserGenerator;
+// import org.OpenNI.Context;
+// import org.OpenNI.GeneralException;
+// import org.OpenNI.OutArg;
+// import org.OpenNI.ScriptNode;
+// import org.OpenNI.UserGenerator;
 
 public class BoneVampire extends Frame {
     
@@ -15,24 +20,24 @@ public class BoneVampire extends Frame {
     
     private Display display = null;
     
-    private Context context = null;
-    private OutArg<ScriptNode> scriptNode = new OutArg<ScriptNode>();
-    private UserGenerator userGen = null;
+    // private Context context = null;
+    // private OutArg<ScriptNode> scriptNode = new OutArg<ScriptNode>();
+    // private UserGenerator userGen = null;
     
     public BoneVampire() {
         
         super("BoneVampire");
         
-        final String CONTEXT_DEF = "config/OpenNI-Config.xml";
+        // final String CONTEXT_DEF = "config/OpenNI-Config.xml";
         
         // Init OpenNI
-        try {
-            context = Context.createFromXmlFile(CONTEXT_DEF, scriptNode);
-            userGen = UserGenerator.create(context);
-        }
-        catch (GeneralException e) {
-            System.err.print(e);
-        }
+        // try {
+        // context = Context.createFromXmlFile(CONTEXT_DEF, scriptNode);
+        // userGen = UserGenerator.create(context);
+        // }
+        // catch (GeneralException e) {
+        // System.err.print(e);
+        // }
         
         // Init window
         display = new Display();
@@ -41,6 +46,17 @@ public class BoneVampire extends Frame {
         setResizable(true);
         setLayout(new BorderLayout());
         add(display, BorderLayout.CENTER);
+        
+        addWindowListener(new WindowAdapter() {
+            
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Window window = e.getWindow();
+                window.setVisible(false);
+                window.dispose();
+                System.exit(0);
+            }
+        });
         
         display.init();
         setVisible(true);
--- BoneVampire/src/com/knowlogik/bonevampire/Display.java
+++ BoneVampire/src/com/knowlogik/bonevampire/Display.java
@@ -1,5 +1,9 @@
 package com.knowlogik.bonevampire;
 
+import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
 import com.knowlogik.bonevampire.camera.PeasyCamPlus;
 
 import processing.core.PApplet;
@@ -8,14 +12,16 @@ public class Display extends PApplet {
     
     private static final long serialVersionUID = 1L;
     
-    private static final int WIDTH = 1200;
-    private static final int HEIGHT = 800;
+    private static final int INIT_WIDTH = 1200;
+    private static final int INIT_HEIGHT = 800;
+    private int canvasWidth = INIT_WIDTH;
+    private int canvasHeight = INIT_HEIGHT;
     
     private PeasyCamPlus cam;
     
     public void setup() {
         
-        size(WIDTH, HEIGHT, P3D);
+        size(INIT_WIDTH, INIT_HEIGHT, P3D);
         background(0);
         
         // smooth();
@@ -33,14 +39,18 @@ public class Display extends PApplet {
         
         // This sets our current view as the default camera for a reset event
         cam.setDefaultCameraState();
+        
+        // Resize help
+        registerPre(this);
     }
     
-    private int count = 0;
+    public void pre() {
+        if (width != canvasWidth || height != canvasHeight) {
+            resizeFrame();
+        }
+    }
     
     public void draw() {
-        
-        // Increment the count
-        count += 1;
         
         // Wipe bg
         background(0);
@@ -93,48 +103,55 @@ public class Display extends PApplet {
         }
     }
     
+    void resizeFrame() {
+        canvasHeight = getHeight();
+        canvasWidth = getWidth();
+        setPreferredSize(new Dimension(canvasWidth, canvasHeight));
+        cam.reset();
+    }
+    
     // private PGraphics buffer = createGraphics(width, height, P3D);
     // float angle;
     // float cosine;
     // float val = 0;
     
-    public void draw2() {
-        
-        // Fade everything
-        // NOTE This doesn't really work using P3D.. Need to figure out a way to fill a volume.
-        // noStroke();
-        // fill(0, 0, 0, 10);
-        // rect(0, 0, width, height);
-        
-        // // Draw ellipses
-        // val += 10;
-        // stroke(196, 5, 23); // Stroke Color of the ellipse and rectangle/red
-        // noFill(); // No fill color for the ellipse
-        // println(mouseX); // Print in the ellipse at mouseX position
-        //
-        // pushMatrix();
-        // translate(mouseX, mouseY); // Translate the center of ellipse around mouseX, mouseY
-        // ellipseMode(CENTER);
-        // rotate(val); // Rotate ellipse around the rising value of 10
-        // ellipse(0, 0, 100, 200); // size and position of ellipse
-        // popMatrix();
-        //
-        // // Draw rectangles in a buffer so they don't get faded
-        // if (second() % 2 == 0) {
-        // cosine = cos(val);
-        //
-        // buffer.beginDraw();
-        // buffer.stroke(196, 5, 23);
-        // buffer.noFill();
-        // buffer.translate(mouseX, mouseY);
-        // buffer.rotate(cosine);
-        // buffer.translate(width / 2, height / 2);
-        // // Translating the the width and height, distance the rect. spins away from ellipse.
-        // buffer.rectMode(CENTER);
-        // buffer.rect(0, 0, 115, 115); // Rect. size and position
-        // buffer.endDraw();
-        // }
-        //
-        // image(buffer, 0, 0);
-    }
+    // public void draw2() {
+    //
+    // // Fade everything
+    // // NOTE This doesn't really work using P3D.. Need to figure out a way to fill a volume.
+    // noStroke();
+    // fill(0, 0, 0, 10);
+    // rect(0, 0, width, height);
+    //
+    // // Draw ellipses
+    // val += 10;
+    // stroke(196, 5, 23); // Stroke Color of the ellipse and rectangle/red
+    // noFill(); // No fill color for the ellipse
+    // println(mouseX); // Print in the ellipse at mouseX position
+    //
+    // pushMatrix();
+    // translate(mouseX, mouseY); // Translate the center of ellipse around mouseX, mouseY
+    // ellipseMode(CENTER);
+    // rotate(val); // Rotate ellipse around the rising value of 10
+    // ellipse(0, 0, 100, 200); // size and position of ellipse
+    // popMatrix();
+    //
+    // // Draw rectangles in a buffer so they don't get faded
+    // if (second() % 2 == 0) {
+    // cosine = cos(val);
+    //
+    // buffer.beginDraw();
+    // buffer.stroke(196, 5, 23);
+    // buffer.noFill();
+    // buffer.translate(mouseX, mouseY);
+    // buffer.rotate(cosine);
+    // buffer.translate(width / 2, height / 2);
+    // // Translating the the width and height, distance the rect. spins away from ellipse.
+    // buffer.rectMode(CENTER);
+    // buffer.rect(0, 0, 115, 115); // Rect. size and position
+    // buffer.endDraw();
+    // }
+    //
+    // image(buffer, 0, 0);
+    // }
 }
